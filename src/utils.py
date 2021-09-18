5b303d2c729b3d3ee20ff5cf74ac2556101785fb
Shendye46 <shendye46@yahoo.com>
2021-09-19 00:25:33 +0200

    FIx pore collision

--- src/utils.py
+++ src/utils.py
@@ -10,4 +10,4 @@ def euclideanDistance(atomA:atom, atomB:atom)->float:
     return math.sqrt(math.pow((atomA.x - atomB.x),2)+math.pow((atomA.y - atomB.y),2)+math.pow((atomA.z - atomB.z),2))
 
 def areColliding(atomA:atom, atomB:atom)->bool:
-    return euclideanDistance(atomA,atomB)<(atomA.radius+atomB.radius)
+    return euclideanDistance(atomA,atomB)>(atomA.radius+atomB.radius)
